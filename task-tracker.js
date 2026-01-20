10b0664d062e04c9599043c6b98c7c0d23710a35
Unidirectrix <93390281+unidirectrix@users.noreply.github.com>
2026-01-20 20:46:12 +0800

    Add 'add' subcommand

--- task-tracker.js
+++ task-tracker.js
@@ -1,4 +1,8 @@
 import { argv } from 'node:process';
+import { existsSync } from 'node:fs';
+import { writeFile, readFile } from 'node:fs/promises'; 
+
+let idCount = 0;
 
 // define list of commands
 // must equal to 'task-cli'
@@ -8,7 +12,7 @@ if (rootCommand !== 'task-cli') {
 }
 
 // legal commands
-const subCommand = {
+const subCommands = {
     "add": addTask,
     "update": updateTask,
     "delete": deleteTask,
@@ -18,7 +22,54 @@ const subCommand = {
     "mark-todo": markTaskTodo
 };
 
-function addTask(){
+const subCommand = argv[3];
+if (!(subCommand in subCommands)){
+    throw new Error("Not a valid subcommand");
+}
+
+if(subCommand === "add"){
+    const description = argv[4];
+    if(description === undefined){
+        throw new Error('Error. Perhaps you\'re missing an input?');
+    }
+    subCommands["add"](description);
+}
+
+async function addTask(description){
+    const now = new Date().toISOString();
+    const task = {
+        "id": 0, 
+        "description": description,
+        "status": 'todo',
+        "createdAt": now,
+        "updatedAt": now
+    };
+
+    let tasks = [];
+
+    let fileExist = existsSync('tasks.json');
+    console.log(fileExist);
+
+    if (!fileExist){
+        try {
+            tasks.push(task);
+            await writeFile('./tasks.json', JSON.stringify(tasks));
+            console.log('Task created successfully.');
+        } catch (err) {
+            console.log(`Error: ${err}`);
+        }
+    } else {
+        try {
+            const file = await readFile('./tasks.json');
+            tasks = JSON.parse(file);
+            task["id"] = Object.keys(tasks).filter(key => key === "id").length + 1;
+            tasks.push(task);
+            await writeFile('./tasks.json', JSON.stringify(tasks));
+            console.log('Task created successfully.');
+        } catch (err) {
+            console.log(`Error: ${err}`);
+        }
+    }
 }
 
 function updateTask(){
